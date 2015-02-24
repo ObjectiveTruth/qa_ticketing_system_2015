edf577ddb4548ba7d97aa1510b1b645a61e4a3e8
ObjectiveTruth-Laptop <josemiguel.mendez@uoit.net>
2015-02-24 16:00:50 -0500

    Adding Accounts.cpp for backend, instantiate Accounts and use Accounts::get(string) to search for a user in the toy example, accounts.txt, will return an Accounts object you can look for ::type and ::credit

---
+++ Accounts.cpp
@@ -0,0 +1,64 @@
+#include <string.h>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Account{
+    public:
+        int type;
+        double credit;
+};
+
+class Accounts{
+    public:
+        Account get(string);
+
+};
+
+
+Account Accounts::get(string search){
+    string line;
+    ifstream myfile ("accounts.txt");
+
+    Account newAccount = Account();
+    newAccount.type = -1;
+    newAccount.credit = -1;
+
+    if (myfile.is_open()){
+        
+        while ( getline (myfile,line) ){
+          char str[1024];
+          strcpy(str, line.c_str());
+          char * pch;
+          pch = strtok (str,",");
+          std::string pchConvert(pch);
+          if(search.compare(pchConvert) == 0){
+                //cout << "found it!" << endl;
+                newAccount = Account();
+
+              pch = strtok (NULL,",");
+              newAccount.type = atoi(pch);
+
+              pch = strtok (NULL,",");
+              newAccount.credit = atof(pch);
+              return newAccount;
+          }
+        }
+        myfile.close();
+    }
+    return newAccount;
+}
+
+//  int main(int argc, char* arg[]){
+//
+//
+ //   Accounts accounts = Accounts(); 
+  //  Account foundAccount = accounts.get("jose");
+   // cout << "type is :" << foundAccount.type << endl;
+    //return 0;
+//}
