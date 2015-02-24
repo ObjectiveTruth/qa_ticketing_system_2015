e06dd4ad916965e31e41d06eb14b82b75f01c712
unknown <100571571@UOSL14-90266K5.oncampus.local>
2015-02-23 22:34:43 -0500

    I Create the Create that now is created. Now I'm waiting for Jose to finish, so we can check!

---
+++ Create.cpp
@@ -0,0 +1,82 @@
+#include "stdafx.h"
+#include "Create.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Create::Create(void)
+{
+}
+
+// Check if the user has privilege to create
+bool Create::ItHasPrivilege(void)
+{
+	/*if (currentAccount.type == ADMIN)
+		return true;
+	else{
+		cout << "Error: Not Authorised to create!";
+		return false;
+	}*/
+	return true;
+}
+
+// Check if the userName already exists
+bool Create::CheckIfUsernameExist()
+{
+	bool exist = false;
+
+	cout << "Enter Username:";
+	cin >> userName;
+
+	if (userName == "exist"){
+	//if (Account.get(userName)){
+		cout << "Error: Username Already Exist!";
+		exist = true;
+	}
+
+	return exist;
+}
+
+// Check if the type exists
+bool Create::CheckIfTypeExist()
+{
+	bool isAType = false;
+
+	cout << "Enter User Type:";
+	cin >> userType;
+
+	//if (userType == (ADMIN || BUYER || SELLER || FULL)){
+		isAType = true;
+	//}
+
+	return isAType;
+}
+
+// Check if the amount of money is in the right format
+bool Create::CheckAmount()
+{
+	cout << "Enter Initial Amount:";
+	cin >> initialAmount;
+
+	while (!cin){
+		cout << "ERROR: Please enter the initial amount in the format: XX.XX\n";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> initialAmount;
+	}
+
+	return true;
+}
+
+bool Create::TryToCreate(void)
+{
+	
+	if (!ItHasPrivilege() || CheckIfUsernameExist() || !CheckIfTypeExist() || !CheckAmount()){
+		return false;
+	}
+	else{
+		cout << "New User Created Successfully!";
+		return true;
+	}
+	
+}
